eefa2ffc850e04fdcd53ee9ca527afddb57baa18
Sweetlight95 <princessadesua93@gmail.com>
2022-06-07 19:54:48 +0100

    recent user

--- src/components/contentss/RecentUser.js
+++ src/components/contentss/RecentUser.js
@@ -1,5 +1,5 @@
 import React,  {useState} from 'react'
-import RecentPost from './RecentPost'
+import {MdOutlineUnfoldMore} from 'react-icons/md'
 
 const RecentUser = () => {
    
@@ -7,8 +7,15 @@ const RecentUser = () => {
     <div>
      
         <div style={{display: 'flex'}}>
-        <div>
-
+        <div className="uss">
+          <ul className="userheader">
+            <li className="id">ID<MdOutlineUnfoldMore/></li>
+            <li className="name">TITLE<MdOutlineUnfoldMore /></li>
+            <li className="address">NAME <MdOutlineUnfoldMore /></li>
+            <li className="phone">ID<MdOutlineUnfoldMore/></li>
+            <li className="role">PHONE <MdOutlineUnfoldMore/></li>
+           
+          </ul>
         </div>
         <div style={{ background: "#FFFFFF", boxShadow: "0px 0px 7px 3px rgba(40, 40, 40, 0.03)", borderRadius: "4px"}}>
 
